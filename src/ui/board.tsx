d6483afe1baa4ea87fe0b187a4d2e8d5b0a9e7cf
Tarcísio Pedro <tarcisiopgs@gmail.com>
2026-02-24 09:28:52 -0300

    feat: add kanban Board component

---
+++ src/ui/board.tsx
@@ -0,0 +1,27 @@
+import { Box } from "ink";
+import React from "react";
+import { Column } from "./column.js";
+import type { KanbanCard } from "./state.js";
+
+interface BoardProps {
+	cards: KanbanCard[];
+	labels: {
+		backlog: string;
+		inProgress: string;
+		done: string;
+	};
+}
+
+export function Board({ cards, labels }: BoardProps) {
+	const backlog = cards.filter((c) => c.column === "backlog");
+	const inProgress = cards.filter((c) => c.column === "in_progress");
+	const done = cards.filter((c) => c.column === "done");
+
+	return (
+		<Box flexGrow={1} flexDirection="row">
+			<Column label={labels.backlog} cards={backlog} />
+			<Column label={labels.inProgress} cards={inProgress} />
+			<Column label={labels.done} cards={done} />
+		</Box>
+	);
+}
